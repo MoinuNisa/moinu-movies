e4850e4fadde3c954cd1f9b2b367376a49a88b99
MoinuNisa <moinudeenback.up@gmail.com>
2026-01-04 10:14:15 +0530

    1080p_x264_pack.json

--- plugin.video.moinu/default.py
+++ plugin.video.moinu/default.py
@@ -4,46 +4,67 @@ import sys
 import json
 from urllib.request import urlopen
 
+TMDB_API_KEY = "051ccf72e026820cb53b8b8531b6a2ba"
 JSON_URL = "https://raw.githubusercontent.com/MoinuNisa/moinu-movies/main/1080p_x264_pack.json"
 
 handle = int(sys.argv[1])
+
+def get_tmdb_info(title, year):
+    try:
+        clean_title = title.split("(")[0].strip()
+        query = clean_title.replace(" ", "%20")
+
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&query={query}&year={year}"
+        data = json.loads(urlopen(url).read().decode("utf-8"))
+
+        if not data.get("results"):
+            return {}
+
+        m = data["results"][0]
+
+        poster = m.get("poster_path")
+        backdrop = m.get("backdrop_path")
+
+        return {
+            "plot": m.get("overview", ""),
+            "poster": f"https://image.tmdb.org/t/p/original{poster}" if poster else "",
+            "fanart": f"https://image.tmdb.org/t/p/original{backdrop}" if backdrop else ""
+        }
+
+    except:
+        return {}
 
 try:
     response = urlopen(JSON_URL)
     data = json.loads(response.read().decode("utf-8"))
 
     for movie in data["movies"]:
+        tmdb = get_tmdb_info(movie["title"], movie.get("year", ""))
+
         li = xbmcgui.ListItem(label=movie["title"])
 
-        # Artwork
         li.setArt({
-            "thumb": movie.get("poster", ""),
-            "poster": movie.get("poster", ""),
-            "fanart": movie.get("fanart", "")
+            "thumb": tmdb.get("poster") or movie.get("poster", ""),
+            "poster": tmdb.get("poster") or movie.get("poster", ""),
+            "fanart": tmdb.get("fanart") or movie.get("fanart", "")
         })
 
-        # Info labels
         li.setInfo("video", {
             "title": movie["title"],
             "year": movie.get("year", ""),
-            "plot": movie.get("plot", "")
+            "plot": tmdb.get("plot", "")
         })
 
-        # Context menu
-        context_items = []
+        context = []
 
         if movie.get("trailer"):
-            context_items.append((
+            context.append((
                 "🎬 Trailer",
-                "RunPlugin({})".format(movie["trailer"])
+                f"PlayMedia({movie['trailer']})"
             ))
 
-        context_items.append((
-            "ℹ Movie Info",
-            "Action(Info)"
-        ))
-
-        li.addContextMenuItems(context_items)
+        context.append(("ℹ Movie Info", "Action(Info)"))
+        li.addContextMenuItems(context)
 
         xbmcplugin.addDirectoryItem(
             handle=handle,
@@ -55,4 +76,8 @@ try:
     xbmcplugin.endOfDirectory(handle)
 
 except Exception as e:
-    xbmcgui.Dialog().ok("Moinu Movies ERROR", str(e))
+    xbmcgui.Dialog().notification(
+        "Moinu Movies",
+        "JSON / Network error",
+        xbmcgui.NOTIFICATION_ERROR
+    )
